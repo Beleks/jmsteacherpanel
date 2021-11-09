6c6aeb4ee1a2188a994eeb23d48383d35b7f0b8d
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2021-11-09 15:00:40 +0800

    Create Module.vue

---
+++ src/components/Course/Edit/Content/Module.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="icons">
+      <div>
+        <SvgCourse></SvgCourse>
+      </div>
+      <div>
+        <SvgCourse></SvgCourse>
+      </div>
+    </div>
+
+    <div class="module_heading">
+
+      <div class="icon">
+        <div class="module_title"></div>
+        <div>
+          <SvgCourse></SvgCourse>
+        </div>
+
+        <div class="icon">
+          <SvgCourse></SvgCourse>
+        </div>
+      </div>
+    
+      <div class="module_heading-capture"></div>
+      <div>
+      
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SvgCourse from "@/components/svg/SvgCourse.vue";
+// import SvgCourse from '../../../svg/'
+
+export default {
+  components: {
+    SvgCourse,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
