58cfb6bd942ff14fafc9ad46d46ee67a842bbe79
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2021-11-11 14:34:56 +0800

    Style (#6)
    * small change
    
    * heading markup
    
    добавлен заголовок модуля

--- src/components/Course/Edit/Content/Module.vue
+++ src/components/Course/Edit/Content/Module.vue
@@ -1,58 +1,65 @@
 <template>
-  <div>
-    <div class="icons">
-      <div class="icon">
-        <SvgTeacher/>
-      </div>
-      <div class="icon">
-        <SvgAddModule/>
-      </div>
-    </div>
-    
+  <div class="module">
     <div class="module_heading">
-    <div class="module_title">Название модуля</div>
-      <div class="icon">
-        <div>
-          <SvgArrow/>
+      <div class="icons">
+        <div class="icon">
+          <SvgTeacher />
         </div>
         <div class="icon">
-          <SvgTrash/>
+          <SvgAddModule />
         </div>
       </div>
-    
-      <div class="module_heading-capture">Добавить описание модуля</div>
-      
-    </div>
 
-    <div class="module">
-      <div class="lesson">
+      <div class="full-heading">
+        <div class="title-box">
+          <div class="title-innerbox">
+            <div class="module_title">Название модуля</div>
+            <div class="icon">
+              <SvgArrow />
+            </div>
+          </div>
           <div class="icon">
-            <SvgTeacher/>
+            <SvgTrash />
+          </div>
+        </div>
+
+        <div class="module_heading-capture">Добавить описание модуля</div>
+      </div>
+    </div>
+    <div class="module_list">
+      <div class="lesson">
+        <div class="icon">
+          <SvgTeacher />
+        </div>
+
+        <div class="lesson_content">
+          <div class="icon">
+            <SvgVideo />
           </div>
           <div class="title">Название урока</div>
-          
+
           <div class="edit">редактировать</div>
           <div class="icon">
-              <SvgTrash/>
+            <SvgTrash />
           </div>
+        </div>
       </div>
+
       <div class="add_lesson_icons">
         <div class="icon">
-              <SvgVideo/>
-          </div>
-          <div class="icon">
-              <SvgTask/>
-          </div>
-          <div class="icon">
-              <SvgEdit/>
-          </div>
-          <div class="icon">
-              <SvgVebinar/>
-              
-          </div>
+          <SvgVideo />
+        </div>
+        <div class="icon">
+          <SvgTask />
+        </div>
+        <div class="icon">
+          <SvgEdit />
+        </div>
+        <div class="icon">
+          <SvgVebinar />
+        </div>
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -65,8 +72,6 @@ import SvgVideo from "@/components/svg/SvgVideo.vue";
 import SvgTask from "@/components/svg/SvgTask.vue";
 import SvgEdit from "@/components/svg/SvgEdit.vue";
 import SvgVebinar from "@/components/svg/SvgVebinar.vue";
-
-
 
 // import SvgCourse from '../../../svg/'
 
